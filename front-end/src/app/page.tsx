37957fb0116654ef16125f4bf462d62a59d16e8f
hbidan <hector.bidan@etu.u-bordeaux.fr>
2025-01-29 17:05:47 +0100

    up hec

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -10,15 +10,15 @@ async function getStrapiData(url: string) {
 }
 
 export default async function Home() {
-  const strapiData = await getStrapiData("/api/homepage");
-
+  const strapiData = await getStrapiData("/api/homepage?populate=*");
   const {Titre, Bio, Title_2, Description, Profile_photo} = strapiData.data;
+  console.log(strapiData.data)
 
   return (
     <main className="container mx-auto py-6">
       <button type="button" className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-100">Contactez moi</button>
       {/*<img src={Profile_photo} alt="" /> A retenter en avancant dans le tuto*/}
-      <img className="size-20 rounded" src={Profile_photo} alt="" />
+      <img className="size-20 rounded" src={"http://localhost:1337"+Profile_photo.url} alt="" />
       <h1 className="text-5xl font-bold">{Titre}</h1>
       <h3 className="text-5l font-bold">{Bio}</h3>
       <h2 className="text-2xl font-bold">{Title_2}</h2>
